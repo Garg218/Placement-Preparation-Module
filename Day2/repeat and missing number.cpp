a654c15d8992766b1d6951f4921bdb6ccc7e558d
Garg218 <89938899+Garg218@users.noreply.github.com>
2022-07-05 19:21:42 +0530

    Update repeat and missing number.cpp

--- Day2/repeat and missing number.cpp
+++ Day2/repeat and missing number.cpp
@@ -1,1 +1,31 @@
 
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{
+	// Write your code here 
+	vector<int> v(n+1,0);
+    for(int i=0;i<n;i++)
+    {
+        v[arr[i]]++;
+    }
+    pair<int,int> p;
+    for(int i=1;i<=n;i++)
+    {
+        if(v[i]==0)
+        {
+            p.first=i;
+            break;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(v[i]>1)
+        {
+            p.second=i;
+            break;
+        }
+        
+    }
+    return p;
+}
