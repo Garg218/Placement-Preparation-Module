9262759d83400f9e15a57f6663d664fd4b36097f
Garg218 <89938899+Garg218@users.noreply.github.com>
2022-07-11 15:47:53 +0530

    Create Minimum Sum of Four Digit Number After Splitting Digits.cpp

---
+++ Day8/Minimum Sum of Four Digit Number After Splitting Digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> dig;
+        while(num)
+        {
+            dig.push_back(num%10);
+            
+            num=num/10;
+        }
+        
+        sort(dig.begin(),dig.end());
+        int num1=dig[0]*10+dig[2];
+        int num2=dig[1]*10+dig[3];
+       
+        return num1+num2;
+    }
+};
