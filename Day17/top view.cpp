16b2aaa050a019240e1bcd92ccbc6117d0aa5f69
Garg218 <89938899+Garg218@users.noreply.github.com>
2022-07-20 22:33:07 +0530

    Create top view.cpp

---
+++ Day17/top view.cpp
@@ -0,0 +1,29 @@
+vector<int> topView(Node *root)
+    {
+        //Your code here
+        vector<int> ans;
+        map<int,int> mp;
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            Node* node=q.front().first;
+            int l=q.front().second;
+            q.pop();
+            if(mp.find(l)==mp.end())
+            {
+                mp[l]=node->data;
+            }
+            if(node->left)
+            q.push({node->left,l-1});
+            if(node->right)
+            q.push({node->right,l+1});
+            
+        }
+        for(auto it:mp)
+        ans.push_back(it.second);
+        
+        return ans;
+        
+        
+    }
